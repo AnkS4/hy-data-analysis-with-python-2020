c1f79eeaab0410d5d6cd52e89f897accc9b2ad1c
AnkS4 <anks@tuta.io>
2020-06-04 17:02:05 +0530

    Add solution

--- part01-e11_interleave/src/interleave.py
+++ part01-e11_interleave/src/interleave.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def interleave(*lists):
-    return []
+	l = []
+	for i in zip(*lists):
+		l.extend(i)
+	return l
 
 def main():
     print(interleave([1, 2, 3], [20, 30, 40], ['a', 'b', 'c']))
