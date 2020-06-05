ac8d29573bfe414265e147bd52445f76fbe6a69e
AnkS4 <anks@tuta.io>
2020-06-06 00:08:24 +0530

    Add solution

--- part01-e13_reverse_dictionary/src/reverse_dictionary.py
+++ part01-e13_reverse_dictionary/src/reverse_dictionary.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 
 def reverse_dictionary(d):
-    return {}
+	d2 = {}
+	val = []
+	for _, i in d.items():
+		for i2 in i:
+			val.append(i2)
+	val = list(set(val))
+	for v in val:
+		temp = []
+		for i, j in d.items():
+			if v in j:
+				temp.append(i)
+		d2[v] = temp
+	return d2
 
 def main():
-    pass
+    d={'move': ['liikuttaa'], 'hide': ['piilottaa', 'salata'], 'six': ['kuusi'], 'fir': ['kuusi']}
+    print(reverse_dictionary(d))
 
 if __name__ == "__main__":
     main()
