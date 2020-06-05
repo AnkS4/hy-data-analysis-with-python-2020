719f86a4feec39d3625786505336c14d338c6818
AnkS4 <anks@tuta.io>
2020-06-06 00:59:10 +0530

    Add solution

--- part01-e14_find_matching/src/find_matching.py
+++ part01-e14_find_matching/src/find_matching.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 def find_matching(L, pattern):
-    return []
+	index = []
+	for n, i in enumerate(L):
+		if pattern in i:
+			index.append(n)
+	return index
 
 def main():
-    pass
+    index = find_matching(["sensitive", "engine", "rubbish", "comment"], "en")
+    print(index)
 
 if __name__ == "__main__":
     main()
