4659fc8d3ad4561da231f204c027867fa8d237ae
AnkS4 <anks@tuta.io>
2020-06-06 19:04:57 +0530

    Add solution

--- part01-e16_transform/src/transform.py
+++ part01-e16_transform/src/transform.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 def transform(s1, s2):
-    return []
+	s1 = map(int, s1.split())
+	s2 = map(int, s2.split())
+	t = [i*j for i,j in zip(s1, s2)]
+	return t
 
 def main():
-    pass
+    print(transform("1 5 3", "2 6 -1"))
 
 if __name__ == "__main__":
     main()
