18eebc97dd338ff8e0259a1010dbc42605d069cc
AnkS4 <anks@tuta.io>
2020-06-06 18:43:14 +0530

    Add solution

--- part01-e15_two_dice_comprehension/src/two_dice_comprehension.py
+++ part01-e15_two_dice_comprehension/src/two_dice_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
+    l = [(i,j) for i in range(1,7) for j in range(1,7) if i+j==5]
+    for i in l:
+    	print(i)
 
 if __name__ == "__main__":
     main()
