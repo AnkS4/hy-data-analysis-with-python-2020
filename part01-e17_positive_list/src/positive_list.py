29b9c2c25a55e74757781060207171a221a83a97
AnkS4 <anks@tuta.io>
2020-06-06 19:20:57 +0530

    Add solution

--- part01-e17_positive_list/src/positive_list.py
+++ part01-e17_positive_list/src/positive_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 def positive_list(L):
-    return []
+	return list(filter(lambda x: x>0, L))
 
 def main():
-    pass
+    print(positive_list([2,-2,0,1,-7]))
 
 if __name__ == "__main__":
     main()
