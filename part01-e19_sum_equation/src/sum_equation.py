01a961e3f220062fe2544d6005633486337808a4
AnkS4 <anks@tuta.io>
2020-06-07 13:09:00 +0530

    Add solution

--- part01-e19_sum_equation/src/sum_equation.py
+++ part01-e19_sum_equation/src/sum_equation.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
 def sum_equation(L):
-    return ""
+	if not L:
+		s = "0 = 0"
+	else:
+		s = " + ".join(str(i) for i in L)
+		s += f" = {str(sum(L))}"
+		'''
+		s = list(map(str, L))
+		s = " + ".join(s) + f" = {sum(L)}"
+		'''
+	return s
 
 def main():
-    pass
+    print(sum_equation([1,5,7]))
 
 if __name__ == "__main__":
     main()
