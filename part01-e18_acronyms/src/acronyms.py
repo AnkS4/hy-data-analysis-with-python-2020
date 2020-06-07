aa0ab7860157c5dfcc9b6a1831c2644c382bcf41
AnkS4 <anks@tuta.io>
2020-06-07 12:02:40 +0530

    Add solution

--- part01-e18_acronyms/src/acronyms.py
+++ part01-e18_acronyms/src/acronyms.py
@@ -2,7 +2,8 @@
 
 
 def acronyms(s):
-    return []
+	s = [i.strip('(,),.') for i in s.split()]
+	return [i for i in s if len(i)>=2 and i.isupper()]
 
 def main():
     print(acronyms("""For the purposes of the EU General Data Protection Regulation (GDPR), the controller of your personal information is International Business Machines Corporation (IBM Corp.), 1 New Orchard Road, Armonk, New York, United States, unless indicated otherwise. Where IBM Corp. or a subsidiary it controls (not established in the European Economic Area (EEA)) is required to appoint a legal representative in the EEA, the representative for all such cases is IBM United Kingdom Limited, PO Box 41, North Harbour, Portsmouth, Hampshire, United Kingdom PO6 3AU."""))
