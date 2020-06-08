5ce08261c81e4e1a21c464ff1f1374bcf5175320
AnkS4 <anks@tuta.io>
2020-06-09 01:03:44 +0530

    Add solution for part02-e01

--- part02-e01_integers_in_brackets/src/integers_in_brackets.py
+++ part02-e01_integers_in_brackets/src/integers_in_brackets.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
+import re
 
 def integers_in_brackets(s):
-    return []
+	mo = re.findall(r'\[\s*([+-]?\d+)\s*\]', s)
+	mo = [int(i) for i in mo]
+	#mo = list(map(int, mo))
+	return mo
 
 def main():
-    pass
+    print(integers_in_brackets("  afd [asd] [12 ] [a34]  [ -43 ]tt [+12]xxx"))
 
 if __name__ == "__main__":
     main()
