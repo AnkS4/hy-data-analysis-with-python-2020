462c031dae1dc7590b26c9063c3e5c1b45dea497
AnkS4 <anks@tuta.io>
2020-06-10 00:54:04 +0530

    Add solution for part02-e02

--- part02-e02_file_listing/src/file_listing.py
+++ part02-e02_file_listing/src/file_listing.py
@@ -4,10 +4,17 @@ import re
 
 
 def file_listing(filename="src/listing.txt"):
-    return []
+	res = []
+	with open(filename, 'r') as f:
+		for l in f:
+			temp = re.findall(r'\S+\s\d\s\w+\s\S+\s+(\d+)\s([A-Za-z]{3})\s+(\d{1,})\s(\d{2}):(\d{2})\s(.+)', l)
+			#r".{10}\s+\d+\s+.+\s+.+\s+(\d+)\s+(...)\s+(\d+)\s+(\d\d):(\d\d)\s+(.+)"
+			temp = (int(temp[0][0]), temp[0][1], int(temp[0][2]), int(temp[0][3]), int(temp[0][4]), temp[0][5])
+			res.append(temp)
+	return res
 
 def main():
-    pass
+	print(file_listing())
 
 if __name__ == "__main__":
     main()
