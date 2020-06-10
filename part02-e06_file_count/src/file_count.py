bdec92af899e2f0dcff3507ac68f9552e7ea5ec3
AnkS4 <anks@tuta.io>
2020-06-10 17:12:17 +0530

    Add solution for part02-e06

--- part02-e06_file_count/src/file_count.py
+++ part02-e06_file_count/src/file_count.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 def file_count(filename):
-    return (0, 0, 0)
+	lines = 0
+	words = 0
+	chars = 0
+	with open(filename, 'r') as f:
+		for i in f:
+			lines += 1
+			w = re.findall(r'(\S+)', i)
+			words += len(w) #len(i.split())
+			chars += len(i) #sum(len(i) for i in w)
+	return (lines, words, chars)
 
 def main():
-    pass
+    for i in sys.argv[1:]:
+    	l, w, c = file_count(i)
+    	print(f"{l}\t{w}\t{c}\t{i}")
 
 if __name__ == "__main__":
     main()
