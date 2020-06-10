c8a72fea3178695b78197f4c676bc1dd3c7c19d6
AnkS4 <anks@tuta.io>
2020-06-10 12:43:27 +0530

    Add solution for part02-e03

--- part02-e03_red_green_blue/src/red_green_blue.py
+++ part02-e03_red_green_blue/src/red_green_blue.py
@@ -3,11 +3,19 @@
 import re
 
 def red_green_blue(filename="src/rgb.txt"):
-    return []
-
+	with open(filename, 'r') as f:
+		res = []
+		for n,i in enumerate(f):
+			if n!=0:
+				pattern = r'(\s*\d{1,3})\s+(\d{1,3})\s+(\d{1,3})\s+(.+)' #(\w+|\w+\s+\w+)
+				temp = re.match(pattern, i)
+				temp = temp.groups()
+				temp = f'{temp[0]}\t{temp[1]}\t{temp[2]}\t{temp[3]}'
+				res.append(temp)
+	return res
 
 def main():
-    pass
+    print(red_green_blue())
 
 if __name__ == "__main__":
     main()
