e4aaa65bbae18bbf81baf64b2ec0597da4d3c308
AnkS4 <anks@tuta.io>
2020-06-10 16:40:09 +0530

    Add solution for part02-e05

--- part02-e05_summary/src/summary.py
+++ part02-e05_summary/src/summary.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 
 import sys
+import re
+import math
 
 def summary(filename):
-    return (0,0,0)
+	with open(filename, 'r') as file:
+		t = file.read()
+		mo = re.findall(r'([+|-]*\d+.?\d*)',t)
+		a = [float(i) for i in mo]
+		l = len(a)
+		total = sum(a)
+		avg = total/l
+		temp = [(i-avg)**2 for i in a]
+		std = math.sqrt(sum(temp)/(l-1))
+	return (total, avg, std)
 
 def main():
-    pass
+	for i in sys.argv[1:]:
+		total, avg, std = summary(i)
+		print(f"File: {i} Sum: {total:.6f} Average: {avg:.6f} Stddev: {std:.6f}")
 
 if __name__ == "__main__":
     main()
