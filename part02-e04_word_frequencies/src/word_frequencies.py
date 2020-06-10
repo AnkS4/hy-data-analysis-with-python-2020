d19e2f96f92ed1d6ef75d2a3d6b3804bf7b241b2
AnkS4 <anks@tuta.io>
2020-06-10 13:08:30 +0530

    Add solution for part02-e04

--- part02-e04_word_frequencies/src/word_frequencies.py
+++ part02-e04_word_frequencies/src/word_frequencies.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 
+import re
+
 def word_frequencies(filename):
-    return {}
+	res = {}
+	with open(filename, 'r') as f:
+		for i in f:
+			temp = re.findall(r'(\S*)', i)
+			temp = [i.strip("""!"#$%&'()*,-./:;?@[]_""") for i in temp]
+			for i in temp:
+				#i!=''
+				if i not in res:
+					res[i] = 1
+				else:
+					res[i] += 1
+	return res
 
 def main():
-    pass
+    print(word_frequencies('src/alice.txt'))
 
 if __name__ == "__main__":
     main()
