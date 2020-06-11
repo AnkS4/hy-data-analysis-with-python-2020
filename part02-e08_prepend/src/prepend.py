ebbfdc925dbb34383b3932f87f00ca9ee71a82e7
AnkS4 <anks@tuta.io>
2020-06-11 23:55:19 +0530

    Add solution for part02-e08

--- part02-e08_prepend/src/prepend.py
+++ part02-e08_prepend/src/prepend.py
@@ -2,9 +2,15 @@
 
 class Prepend(object):
     # Add the methods of the class here
+    def __init__(self, start):
+    	self.start = start
+
+    def write(self, s):
+    	print(self.start+s)
 
 def main():
-    pass
+    p = Prepend("+++ ")
+    p.write("Hello")
 
 if __name__ == "__main__":
     main()
