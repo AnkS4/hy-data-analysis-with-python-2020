95492c74f2f3a1396f35b6c9407ab416fc5d484c
AnkS4 <anks@tuta.io>
2020-06-12 22:33:50 +0530

    Add solution for part02-e09

--- part02-e09_rational/src/rational.py
+++ part02-e09_rational/src/rational.py
@@ -1,7 +1,38 @@
 #!/usr/bin/env python3
 
 class Rational(object):
-    pass
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def __str__(self):
+        return f"{self.a}/{self.b}"
+    def __mul__(num1, num2):
+        return Rational(num1.a*num2.a, num1.b*num2.b)
+    def __truediv__(num1, num2):
+        return Rational(num1.a*num2.b, num1.b*num2.a)
+    def __add__(num1, num2):
+        return Rational(num1.a*num2.b + num2.a*num1.b, num1.b*num2.b)
+    def __sub__(num1, num2):
+        return Rational(num1.a*num2.b - num2.a*num1.b, num1.b*num2.b)
+    def __eq__(num1, num2):
+        return num1.a == num2.a and num1.b == num2.b
+        #num1.a*num2.b == num2.a*num1.b
+    def __gt__(num1, num2):
+        return num1.a*num2.b > num2.a*num1.b
+        '''
+        if num1.b == num2.b:
+            return num1.b > num2.b
+        else:
+            return num1.a*num2.b > num2.a*num1.b
+        '''
+    def __lt__(num1, num2):
+        return num1.a*num2.b < num2.a*num1.b
+        '''
+        if num1.b == num2.b:
+            return num1.b < num2.b
+        else:
+            return num1.a*num2.b < num2.a*num1.b
+        '''
 
 def main():
     r1=Rational(1,4)
