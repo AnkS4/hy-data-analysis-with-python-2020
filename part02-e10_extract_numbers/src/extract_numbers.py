41f4d5dc00c3b6cb9021a93fda91fc60610c27a9
AnkS4 <anks@tuta.io>
2020-06-13 00:34:02 +0530

    Add solution for part02-e10

--- part02-e10_extract_numbers/src/extract_numbers.py
+++ part02-e10_extract_numbers/src/extract_numbers.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
+import re
+
 def extract_numbers(s):
-    return []
+    #s = re.findall(r'([+-]?\d+[.]?\d*)',s)
+    s = s.split()
+    l = []
+    for i in s:
+        try:
+            l.append(int(i))
+        except (ValueError):
+            try:
+                l.append(float(i))
+            except Exception:
+                pass
+    return l
 
 def main():
     print(extract_numbers("abd 123 1.2 test 13.2 -1"))
