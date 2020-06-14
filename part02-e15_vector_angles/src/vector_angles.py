f03b380f2d56c32eb68f1d91024f0674eb425ddb
AnkS4 <anks@tuta.io>
2020-06-14 23:09:36 +0530

    Add solution for part02-e15

--- part02-e15_vector_angles/src/vector_angles.py
+++ part02-e15_vector_angles/src/vector_angles.py
@@ -4,10 +4,14 @@ import numpy as np
 import scipy.linalg
 
 def vector_angles(X, Y):
-    np.array([])
+    A = np.sum(X*Y, axis=1)
+    B = np.sqrt(np.sum(np.square(X), axis=1)) * np.sqrt(np.sum(np.square(Y), axis=1))
+    return np.degrees(np.arccos(np.clip(A/B, -1.0, 1.0)))
 
 def main():
-    np.array([])
+    X = np.array([[1, 4, 3, 7], [0, 2, 2, 1]])
+    Y = np.array([[2, 7, 5, 2], [8, 5, 1, 0]])
+    print(vector_angles(X, Y))
 
 if __name__ == "__main__":
     main()
