ab310430d2dcec43fd6bfa1d703d043d697fbc92
AnkS4 <anks@tuta.io>
2020-06-14 14:46:28 +0530

    Optimize solution

--- part02-e13_diamond/src/diamond.py
+++ part02-e13_diamond/src/diamond.py
@@ -3,6 +3,10 @@
 import numpy as np
 
 def diamond(n):
+    a = np.eye(n, dtype=int)
+    a = np.concatenate([a[::-1], a[:,1:]], axis=1)
+    return np.concatenate([a[:-1], a[::-1]], axis=0)
+    '''
     A = np.eye(n, dtype=int)
     B = np.flip(A, axis=0)
     B = np.delete(B, -1, axis=1)
@@ -10,9 +14,10 @@ def diamond(n):
     D = np.flip(C, axis=0)
     D = np.delete(D, 0, axis=0)
     return np.concatenate((C, D), axis=0)
+    '''
 
 def main():
-    print(diamond(1))
+    print(diamond(4))
 
 if __name__ == "__main__":
     main()
