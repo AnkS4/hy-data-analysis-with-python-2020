a20b6ae6c43b02064e3518c79733e8521cf33626
AnkS4 <anks@tuta.io>
2020-06-14 14:38:37 +0530

    Add solution for part02-e13

--- part02-e13_diamond/src/diamond.py
+++ part02-e13_diamond/src/diamond.py
@@ -3,10 +3,16 @@
 import numpy as np
 
 def diamond(n):
-    return np.array([])
+    A = np.eye(n, dtype=int)
+    B = np.flip(A, axis=0)
+    B = np.delete(B, -1, axis=1)
+    C = np.concatenate((B, A), axis=1)
+    D = np.flip(C, axis=0)
+    D = np.delete(D, 0, axis=0)
+    return np.concatenate((C, D), axis=0)
 
 def main():
-    pass
+    print(diamond(1))
 
 if __name__ == "__main__":
     main()
