ec8e4085f1c5a38e49176e6fa872bc125757985a
AnkS4 <anks@tuta.io>
2020-06-14 20:44:43 +0530

    Add solution for part02-e14

--- part02-e14_vector_lengths/src/vector_lengths.py
+++ part02-e14_vector_lengths/src/vector_lengths.py
@@ -4,10 +4,13 @@ import numpy as np
 #import scipy.linalg
 
 def vector_lengths(a):
-    return np.array([])
+    return np.sqrt(np.sum(np.square(a), axis=1))
+    #[np.sum(np.sqrt(i**i)) for i in a]
 
 def main():
-    pass
+    a = np.array([[5, 0, 3],
+                  [3, 7, 9]])
+    print(vector_lengths(a))
 
 if __name__ == "__main__":
     main()
