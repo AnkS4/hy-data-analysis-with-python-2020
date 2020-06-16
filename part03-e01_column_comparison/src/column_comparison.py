972ce89c4fdc0d76a9fc6a1dc4eeafd86190da48
AnkS4 <anks@tuta.io>
2020-06-16 16:42:47 +0530

    Add solution for part03-01

--- part03-e01_column_comparison/src/column_comparison.py
+++ part03-e01_column_comparison/src/column_comparison.py
@@ -3,10 +3,15 @@
 import numpy as np
 
 def column_comparison(a):
-    return np.array([])
+    return a[a[:,1] > a[:,-2]]
     
 def main():
-    pass
+    a = np.array([[8, 9, 3, 8, 8],
+                  [0, 5, 3, 9, 9],
+                  [5, 7, 6, 0, 4],
+                  [7, 8, 1, 6, 2],
+                  [2, 1, 3, 5, 8]])
+    print(column_comparison(a))
 
 if __name__ == "__main__":
     main()
