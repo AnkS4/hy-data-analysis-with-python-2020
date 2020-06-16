ef54ad1cf77ae36694cb3ffea285fef64455dfda
AnkS4 <anks@tuta.io>
2020-06-16 17:30:21 +0530

    Add solution for part03-e02

--- part03-e02_first_half_second_half/src/first_half_second_half.py
+++ part03-e02_first_half_second_half/src/first_half_second_half.py
@@ -3,10 +3,13 @@
 import numpy as np
 
 def first_half_second_half(a):
-    return np.array([])
+    l = a.shape[1]//2
+    return a[np.sum(a[:,:l], axis=1)>np.sum(a[:,l:], axis=1)]
 
 def main():
-    pass
+    a = np.array([[1, 3, 4, 2],
+                  [2, 2, 1, 2]])
+    print(first_half_second_half(a))
 
 if __name__ == "__main__":
     main()
