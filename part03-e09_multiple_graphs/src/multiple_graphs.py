4dd3ff4a6444b5f01adfe651ab54f21200f53241
AnkS4 <anks@tuta.io>
2020-06-18 23:06:04 +0530

    Add solution for part03-e09

--- part03-e09_multiple_graphs/src/multiple_graphs.py
+++ part03-e09_multiple_graphs/src/multiple_graphs.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    pass
+    x1 = np.array([2,4,6,7])
+    y1 = np.array([4,3,5,1])
+    x2 = np.array([1,2,3,4])
+    y2 = np.array([4,2,3,1])
+    plt.plot(x1, y1)
+    plt.plot(x2, y2)
+    plt.title("title")
+    plt.xlabel("x-axis")
+    plt.ylabel("y-label")
+    plt.show()
 
 if __name__ == "__main__":
     main()
