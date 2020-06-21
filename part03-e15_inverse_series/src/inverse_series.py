f69750282f2fd9e39e9d31310bc145804464e307
AnkS4 <anks@tuta.io>
2020-06-21 13:02:09 +0530

    Add solution for part03-e15

--- part03-e15_inverse_series/src/inverse_series.py
+++ part03-e15_inverse_series/src/inverse_series.py
@@ -3,10 +3,14 @@
 import pandas as pd
 
 def inverse_series(s):
-    return pd.Series()
+    s2 = pd.Series(s.index, s.values)
+    return s2
 
 def main():
-    return
+    s = pd.Series([1, 2, 5, 2, 9], list("abcde"))
+    s2 = inverse_series(s)
+    print(s2)
+    print(s2[2])
 
 if __name__ == "__main__":
     main()
