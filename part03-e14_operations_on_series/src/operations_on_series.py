c553ff44b7612008ff389fc3d1bdc66b66818d39
AnkS4 <anks@tuta.io>
2020-06-21 12:52:55 +0530

    Add solution for part03-e14

--- part03-e14_operations_on_series/src/operations_on_series.py
+++ part03-e14_operations_on_series/src/operations_on_series.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 
+import numpy as np
+import pandas as pd
 
 def create_series(L1, L2):
-    return (None, None)
+    ind = ['a', 'b', 'c'] #list("abc")
+    s1 = pd.Series(L1, ind)
+    s2 = pd.Series(L2, ind)
+    return (s1, s2)
     
 def modify_series(s1, s2):
-    return (None, None)
+    s1['d'] = s2['b']
+    del s2['b']
+    return (s1, s2)
     
 def main():
-    return
+    L1 = [1, 3, 7]
+    L2 = [2, 5, 0]
+    s1, s2 = create_series(L1, L2)
+    print(s1, s2)
+    s1, s2 = modify_series(s1, s2)
+    print(s1, s2)
+    print(s1+s2)
     
 if __name__ == "__main__":
     main()
