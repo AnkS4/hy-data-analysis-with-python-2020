22bfa37ea4feaa62a2589b41be9f26baac090dd3
AnkS4 <anks@tuta.io>
2020-06-22 23:56:57 +0530

    Add solution for part04-e01

--- part04-e01_cities/src/cities.py
+++ part04-e01_cities/src/cities.py
@@ -3,10 +3,13 @@
 import pandas as pd
 
 def cities():
-    return None
+    idx = ["Helsinki", "Espoo", "Tampere", "Vantaa", "Oulu"]
+    pop = pd.Series([643272, 279044, 231853, 223027, 201810], index=idx, name="Population")
+    area = pd.Series([715.48, 528.03, 689.59, 240.35, 3817.52], index=idx, name="Total area")
+    return pd.DataFrame({"Population": pop, "Total area": area})
     
 def main():
-    return
+    print(cities())
     
 if __name__ == "__main__":
     main()
