c14cbe429954f14750a4a8ea18f42eff3eaebe9d
AnkS4 <anks@tuta.io>
2020-06-24 01:21:36 +0530

    Add solution for part04-e02

--- part04-e02_powers_of_series/src/powers_of_series.py
+++ part04-e02_powers_of_series/src/powers_of_series.py
@@ -3,10 +3,11 @@
 import pandas as pd
 
 def powers_of_series(s, k):
-    return None
+    return pd.DataFrame({i:s**i for i in range(1,k+1)})
     
 def main():
-    return
+    s = pd.Series([1,2,3,4], index=list("abcd"))
+    print(powers_of_series(s, 3))
     
 if __name__ == "__main__":
     main()
