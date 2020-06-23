f1c1ba6e9c1af93b3e88ea2f8ca27ce58a2a2edd
AnkS4 <anks@tuta.io>
2020-06-24 02:30:28 +0530

    Add solution for part04-e04

--- part04-e04_municipalities_of_finland/src/municipalities_of_finland.py
+++ part04-e04_municipalities_of_finland/src/municipalities_of_finland.py
@@ -3,10 +3,17 @@
 import pandas as pd
 
 def municipalities_of_finland():
-    return None
+    df = pd.read_csv("src/municipal.tsv", sep="\t", index_col="Region 2018")
+    #index_col=0
+    return df["Akaa":"Äänekoski"]
+    '''
+    df.drop('WHOLE COUNTRY', inplace=True)
+    return df[:311]
+    '''
+    #df[:df.index.get_loc('Äänekoski')+1]
     
 def main():
-    return
+    print(municipalities_of_finland())
     
 if __name__ == "__main__":
     main()
