56a388dfcefde657df46cefa1b528d533ffa91aa
AnkS4 <anks@tuta.io>
2020-06-24 19:46:24 +0530

    Add solution for part04-e06

--- part04-e06_growing_municipalities/src/growing_municipalities.py
+++ part04-e06_growing_municipalities/src/growing_municipalities.py
@@ -3,10 +3,14 @@
 import pandas as pd
 
 def growing_municipalities(df):
-    0.0
+    df2 = df[df["Population change from the previous year, %"]>0]
+    return df2.shape[0]/df.shape[0]
 
 def main():
-    return
+    df = pd.read_csv("src/municipal.tsv", sep="\t", index_col=0)
+    df = df["Akaa":"Äänekoski"]
+    per = growing_municipalities(df)
+    print(f"Proportion of growing municipalities: {per:.1f}%")
 
 if __name__ == "__main__":
     main()
