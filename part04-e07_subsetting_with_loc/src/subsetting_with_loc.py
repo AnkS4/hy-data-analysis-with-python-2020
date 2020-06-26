0a1d2002826af7a358d0e714ec507d89a23ec681
AnkS4 <anks@tuta.io>
2020-06-26 15:12:48 +0530

    Add solution for part04-e07

--- part04-e07_subsetting_with_loc/src/subsetting_with_loc.py
+++ part04-e07_subsetting_with_loc/src/subsetting_with_loc.py
@@ -3,10 +3,11 @@
 import pandas as pd
 
 def subsetting_with_loc():
-    return None
+    df = pd.read_csv("src/municipal.tsv", sep="\t", index_col=0)
+    return df.loc["Akaa":"Äänekoski", ["Population", "Share of Swedish-speakers of the population, %", "Share of foreign citizens of the population, %"]]
 
 def main():
-    return
+    print(subsetting_with_loc())
 
 if __name__ == "__main__":
     main()
