7f669845d522df53150c3cd253c5167384ce11e1
AnkS4 <anks@tuta.io>
2020-06-26 15:59:52 +0530

    Add solution for part04-e11

--- part04-e11_below_zero/src/below_zero.py
+++ part04-e11_below_zero/src/below_zero.py
@@ -3,10 +3,12 @@
 import pandas as pd
 
 def below_zero():
-    return 0
+    df = pd.read_csv("src/kumpula-weather-2017.csv")
+    return sum(df["Air temperature (degC)"]<0)
 
 def main():
-    return
+    sub0 = below_zero()
+    print(f"Number of days below zero: {sub0}")
     
 if __name__ == "__main__":
     main()
