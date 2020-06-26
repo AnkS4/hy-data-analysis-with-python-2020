447ac76cc1e1989aff263b51673296e21b97b562
AnkS4 <anks@tuta.io>
2020-06-26 15:33:13 +0530

    Add solution for part04-e08

--- part04-e08_subsetting_by_positions/src/subsetting_by_positions.py
+++ part04-e08_subsetting_by_positions/src/subsetting_by_positions.py
@@ -3,10 +3,11 @@
 import pandas as pd
 
 def subsetting_by_positions():
-    return pd.DataFrame()
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t", index_col=0)
+    return df.iloc[:10, [1, 2]] #[:10,2:4]
 
 def main():
-    return
+    print(subsetting_by_positions())
 
 if __name__ == "__main__":
     main()
