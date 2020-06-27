82a8dceda9b34ec3cc8d37aff005cb1050899b97
AnkS4 <anks@tuta.io>
2020-06-27 17:44:10 +0530

    Add solution for part04-e13

--- part04-e13_missing_value_types/src/missing_value_types.py
+++ part04-e13_missing_value_types/src/missing_value_types.py
@@ -4,10 +4,12 @@ import pandas as pd
 import numpy as np
 
 def missing_value_types():
-    return None
-               
+    df = pd.DataFrame([["United Kingdom", np.nan, None], ["Finland", 1917, "Niinistö"], ["USA", 1776, "Trump"], ["Sweden", 1523, None], ["Germany", np.nan, "Steinmeier"], ["Russia", 1992, "Putin"]], columns=["State", "Year of independence", "President"])
+    df.set_index("State", inplace=True)
+    return df
+
 def main():
-    return
+    print(missing_value_types())
 
 if __name__ == "__main__":
     main()
