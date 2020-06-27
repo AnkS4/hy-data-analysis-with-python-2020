7abd4a8626804961bff2e708f921cfd7920f48ba
AnkS4 <anks@tuta.io>
2020-06-27 18:54:59 +0530

    Add solution for part04-e14

--- part04-e14_special_missing_values/src/special_missing_values.py
+++ part04-e14_special_missing_values/src/special_missing_values.py
@@ -4,10 +4,14 @@ import pandas as pd
 import numpy as np
 
 def special_missing_values():
-    return None
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    df.replace(["New", "Re"], np.nan, inplace=True)
+    #df.loc[(df["LW"] == "New") | (df["LW"] == "Re"), "LW"] = np.nan
+    df[["Pos", "LW"]] = df[["Pos", "LW"]].astype('float')
+    return df[df["Pos"]> df["LW"]]
 
 def main():
-    return
+    print(special_missing_values())
 
 if __name__ == "__main__":
     main()
