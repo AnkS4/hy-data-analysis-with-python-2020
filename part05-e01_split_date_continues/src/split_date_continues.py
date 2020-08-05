e96b1f559e749ef2b6b3dbaa0e8f949ec70f3d03
AnkS <anks@tuta.io>
2020-08-06 00:34:33 +0530

    Add solution for part05-e01

--- part05-e01_split_date_continues/src/split_date_continues.py
+++ part05-e01_split_date_continues/src/split_date_continues.py
@@ -3,11 +3,30 @@
 import pandas as pd
 
 
-def split_date(df):
-    return df
+def split_date():
+	df = pd.read_csv("src/Helsingin_pyorailijamaarat.csv", sep=";")
+	df2 = df["Päivämäärä"].str.split(expand=True)
+	df2.dropna(inplace=True)
+	df2.columns =  ["Weekday", "Day", "Month", "Year", "Hour"]
+	df2["Hour"] = df2["Hour"].str.split(":", expand=True)[0]
+	
+	days = dict(zip("ma ti ke to pe la su".split(), "Mon Tue Wed Thu Fri Sat Sun".split()))
+	months = dict(zip("tammi helmi maalis huhti touko kesä heinä elo syys loka marras joulu".split(), range(1,13)))
+	
+	df2["Weekday"] = df2["Weekday"].map(days)
+	df2["Month"] = df2["Month"].map(months)
+	df2 = df2.astype({"Weekday": object, "Day": int, "Month": int, "Year": int, "Hour": int})
+	
+	df.drop("Päivämäärä", axis=1, inplace=True)
+	df.dropna(axis=0, how='all', inplace=True)
+	df.dropna(axis=1, how='all', inplace=True)
+	#df.dropna(axis=0, how="all", inplace=True).dropna(axis=1, how="all", inplace=True)
+	df = pd.concat([df2, df], axis=1)
+	return df
 
 def split_date_continues():
-    return None
+	df = split_date()
+	return df
 
 def main():
     df = split_date_continues()
