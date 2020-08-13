23bb78576ad44363e1283f65f663b07e463ff802
AnkS <anks@tuta.io>
2020-08-14 00:20:10 +0530

    Add solution for part05-e03

--- part05-e03_top_bands/src/top_bands.py
+++ part05-e03_top_bands/src/top_bands.py
@@ -3,10 +3,15 @@
 import pandas as pd
 
 def top_bands():
-    return pd.DataFrame()
+	df1 = pd.read_csv("src/bands.tsv", sep="\t")
+	df1["Band"] = df1["Band"].str.upper()
+	df2 = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+	df = pd.merge(df2, df1, left_on='Artist', right_on='Band')
+	return df
 
 def main():
-    return
+	df = top_bands()
+	print(df.head())
 
 if __name__ == "__main__":
     main()
