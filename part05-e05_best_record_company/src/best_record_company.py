4ec7197535f7dfd8980e920ccf534011c5ff873d
AnkS4 <anks@tuta.io>
2020-08-27 22:54:16 +0530

    Add solution for part05-e05

--- part05-e05_best_record_company/src/best_record_company.py
+++ part05-e05_best_record_company/src/best_record_company.py
@@ -3,11 +3,22 @@
 import pandas as pd
 
 def best_record_company():
-    return None
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    #df.groupby("Publisher")[["Publisher","WoC"]].sum() == max(df.groupby("Publisher")[["WoC"]].sum())
+    '''
+    groups = df.groupby("Publisher")
+    num=0
+    for i,j in zip(groups,groups["WoC"].sum()):
+        if j>num:
+            num=j
+            df2=i[1]
+    return df2
+    '''
+    return df[df["Publisher"]== df.groupby("Publisher")["WoC"].sum().idxmax()]
 
 def main():
-    return
+    df = best_record_company()
+    print(df)
     
-
 if __name__ == "__main__":
     main()
