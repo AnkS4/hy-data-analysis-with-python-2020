9890c662f68b2d21cb256631349cded20aaa532c
AnkS4 <anks@tuta.io>
2020-09-01 23:00:50 +0530

    Add solution for part05-e06

--- part05-e06_suicide_fractions/src/suicide_fractions.py
+++ part05-e06_suicide_fractions/src/suicide_fractions.py
@@ -3,10 +3,14 @@
 import pandas as pd
 
 def suicide_fractions():
-    return pd.Series()
+    df = pd.read_csv("src/who_suicide_statistics.csv", index_col="country")
+    df["mean_fraction"] = df["suicides_no"]/df["population"]
+    df2 = df.groupby("country")[["mean_fraction"]].mean()
+    return pd.Series(df2["mean_fraction"])
 
 def main():
-    return
+    df = suicide_fractions()
+    print(df)
 
 if __name__ == "__main__":
     main()
