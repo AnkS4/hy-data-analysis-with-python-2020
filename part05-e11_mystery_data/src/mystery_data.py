383459c258068fa0d90c66494864fef590ceb7c2
AnkS4 <anks@tuta.io>
2020-09-09 15:37:55 +0530

    Add solution for part05-e11

--- part05-e11_mystery_data/src/mystery_data.py
+++ part05-e11_mystery_data/src/mystery_data.py
@@ -4,11 +4,16 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 def mystery_data():
-    return []
+    df = pd.read_csv("src/mystery_data.tsv", sep="\t")
+    model = LinearRegression(fit_intercept=False)
+    model.fit(df.iloc[:,0:5], df.iloc[:,5])
+    return model.coef_
 
 def main():
     coefficients = mystery_data()
     # print the coefficients here
+    for i, co in enumerate(coefficients):
+        print(f"Coefficient of X{i+1} is {co}")
     
 if __name__ == "__main__":
     main()
