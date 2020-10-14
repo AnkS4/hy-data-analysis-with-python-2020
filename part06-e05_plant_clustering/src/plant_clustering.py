52aba08de7e1a76d93c6c33a561604549f1f9a8f
AnkS4 <anks@tuta.io>
2020-10-14 16:01:04 +0530

    Add solution for part06-e05

--- part06-e05_plant_clustering/src/plant_clustering.py
+++ part06-e05_plant_clustering/src/plant_clustering.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import scipy
-
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+from sklearn.cluster import KMeans
 
 def find_permutation(n_clusters, real_labels, labels):
     permutation=[]
@@ -13,7 +15,12 @@ def find_permutation(n_clusters, real_labels, labels):
     return permutation
 
 def plant_clustering():
-    return 0.0
+	iris = load_iris()
+	model=KMeans(3, random_state=0)
+	model.fit(iris.data)
+	permutation = find_permutation(3, iris.target, model.labels_)
+	new_labels = [permutation[label] for label in model.labels_]
+	return accuracy_score(iris.target, new_labels)
 
 def main():
     print(plant_clustering())
