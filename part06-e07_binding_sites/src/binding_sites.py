6f207e495ab67d1f9a48300e12e921cb828dbc10
AnkS4 <anks@tuta.io>
2020-10-29 00:30:14 +0530

    Add solution for part06-e07

--- part06-e07_binding_sites/src/binding_sites.py
+++ part06-e07_binding_sites/src/binding_sites.py
@@ -13,11 +13,29 @@ sns.set(color_codes=True)
 import scipy.spatial as sp
 import scipy.cluster.hierarchy as hc
 
+import scipy
+
+def find_permutation(n_clusters, real_labels, labels):
+    permutation=[]
+    for i in range(n_clusters):
+        idx = labels == i
+        # Choose the most common label among data points in the cluster
+        new_label=scipy.stats.mode(real_labels[idx])[0][0]
+        permutation.append(new_label)
+    return permutation
+
 def toint(x):
-    return 0
+	#d=dict(zip("ACGT", range(4)))
+	#return d[x]
+	return 'ACGT'.find(x)
 
 def get_features_and_labels(filename):
-    return (np.array([[]]), np.array([]))
+	df = pd.read_csv(filename, sep="\t")
+	X = [[toint(j) for j in i] for i in df["X"]]
+	#X = np.array(df.X.map(list).values.tolist())
+	#toint2 = np.vectorize(toint)
+	#X = toint2(A)
+	return (np.array(X), df.y) #df[["y"]]
 
 def plot(distances, method='average', affinity='euclidean'):
     mylinkage = hc.linkage(sp.distance.squareform(distances), method=method)
@@ -26,11 +44,20 @@ def plot(distances, method='average', affinity='euclidean'):
     plt.show()
 
 def cluster_euclidean(filename):
-    return 0.0
+	X, y = get_features_and_labels(filename)
+	model = AgglomerativeClustering(linkage="average", affinity="euclidean").fit(X)
+	permutation = find_permutation(2, y, model.labels_)
+	#d = pairwise_distances(X, metric="euclidean")
+	#plot(d, "average", "euclidean")
+	return accuracy_score(y, [permutation[label] for label in model.labels_])
 
 def cluster_hamming(filename):
-    return 0.0
-
+	X, y = get_features_and_labels(filename)
+	d = pairwise_distances(X, metric="hamming")
+	model = AgglomerativeClustering(linkage="average", affinity="precomputed").fit(d)
+	permutation = find_permutation(2, y, model.labels_)
+	#plot(d, "average", "hamming")
+	return accuracy_score(y, [permutation[label] for label in model.labels_])
 
 def main():
     print("Accuracy score with Euclidean affinity is", cluster_euclidean("src/data.seq"))
